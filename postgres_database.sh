915c30921eb91e9c4a2a42bd28ad8c80ecb45b0d
Tim Galvin <tim.galvin@curtin.edu.au>
2022-10-06 10:56:38 +0800

    tweaks to layout / getopts for arguments

--- postgres_database.sh
+++ postgres_database.sh
@@ -1,26 +1,67 @@
-# Need singulaity, and to remove the badness of pawsey
-module load singularity
-unset SINGULARITY_BINDPATH
+#!/bin/bash -l
+
+help="This script will configure the prefect environment, and if requested start the 
+postgres server necessary.
+
+Options:
+    -s  - will attempt to start the postgres server from a singularity container
+    -h  - will print this help page
+
+Usage:
+postgres_database.sh [-s | -h]
+"
+
+START_POSTGRES=1
+
+while getopts 'sh' arg; do 
+    case $arg in
+    s)
+        echo "Will attempt to start postgres server"
+        START_POSTGRES=0
+        ;;
+    *)
+        echo "$help" 
+        exit 1
+        ;;
+    esac
+done
 
 # Now set up some postgres values
 export POSTGRES_PASS=password
-export POSTGRES_ADDR=127.0.0.1
+POSTGRES_ADDR=$(ifconfig ipogif0  | grep 'inet addr' | cut -d ':' -f2 | awk '{print $1}')
+export POSTGRES_ADDR
 export POSTGRES_USER=postgres
 export POSTGRES_DB=orion
-export POSTGRES_SCRATCH=$(pwd)
-
-if [[ ! -e "${POSTGRES_SCRATCH}/pgdata" ]]; then
-    echo "Creating pgdata"
-    mkdir pgdata
-fi
-
-singularity pull docker://postgres  ## singularity pull has to be done only once
-SINGULARITYENV_POSTGRES_PASSWORD=$POSTGRES_PASS SINGULARITYENV_POSTGRES_DB=$POSTGRES_DB SINGULARITYENV_PGDATA=$POSTGRES_SCRATCH/pgdata \
-    singularity run --cleanenv --bind $POSTGRES_SCRATCH:/var postgres_latest.sif &
+POSTGRES_SCRATCH=$(pwd)
+export POSTGRES_SCRATCH
 
 export PREFECT_ORION_DATABASE_CONNECTION_URL="postgresql+asyncpg://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_ADDR:5432/$POSTGRES_DB"
 
+PREFECT_HOME="$(pwd)/prefect"
+export PREFECT_HOME
 
-#export PREFECT_ORION_DATABASE_CONNECTION_URL="sqlite+aiosqlite:////$(pwd)/test_db.db"
 
-export PREFECT_HOME="$(pwd)/prefect"
+if [[ $START_POSTGRES -eq 0 ]]
+then
+    # Need singulaity, and to remove the badness of pawsey
+    echo "Loading the singularity module "
+    module load singularity
+    SINGULARITY_BINDPATH="$POSTGRES_SCRATCH"
+    
+    export SINGULARITY_BINDPATH
+
+
+    if [[ ! -e "${POSTGRES_SCRATCH}/pgdata" ]]; then
+        echo "Creating pgdata for the postgres server operation"
+        mkdir pgdata
+    fi
+
+    if [[ ! -e postgres_latest.sif ]]
+    then
+        echo "Downloading the latest postgres docker container"
+        singularity pull docker://postgres  
+    fi
+    
+    SINGULARITYENV_POSTGRES_PASSWORD="$POSTGRES_PASS" SINGULARITYENV_POSTGRES_DB="$POSTGRES_DB" SINGULARITYENV_PGDATA="$POSTGRES_SCRATCH/pgdata" \
+        singularity run --cleanenv --bind "$POSTGRES_SCRATCH":/var postgres_latest.sif 
+fi
